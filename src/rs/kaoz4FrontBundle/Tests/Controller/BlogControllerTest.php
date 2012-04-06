0b53467f4a14e07b68e7ae6254a6024b7a393306
digitalkaoz <caziel@gmx.net>
2012-04-06 11:35:35 +0200

    layout fixes

--- src/rs/kaoz4FrontBundle/Tests/Controller/BlogControllerTest.php
+++ src/rs/kaoz4FrontBundle/Tests/Controller/BlogControllerTest.php
@@ -9,32 +9,32 @@ class BlogControllerTest extends BaseControllerTest
     public function testIndex()
     {
         $session = $this->getSession('symfony');
-        $session->visit($this->base.'/blog');
+        $session->visit($this->base . '/blog');
 
         $this->assertEquals($session->getStatusCode(), 200);
-        
-        foreach($this->getPosts() as $post)
+
+        foreach ($this->getPosts() as $post)
         {
-            $this->assertTrue($session->getPage()->has('css', 'a[href="/blog/article/'.$post->getSlug().'"]'));
-        }        
+            $this->assertTrue($session->getPage()->has('css', 'a[href="/blog/article/' . $post->getSlug() . '"]'));
+        }
     }
-    
+
     public function testDetail()
     {
         $session = $this->getSession('symfony');
 
-        foreach($this->getPosts() as $post)
+        foreach ($this->getPosts() as $post)
         {
-            $session->visit($this->base.'/blog/article/'.$post->getSlug());
-            
-            $this->assertEquals($session->getStatusCode(), 200);            
-            $this->assertEquals($session->getPage()->find('css','.main h2')->getText(),$post->getTitle());
-            $this->assertTrue($session->getPage()->has('css','#disqus_thread'));
-        }        
+            $session->visit($this->base . '/blog/article/' . $post->getSlug());
+
+            $this->assertEquals($session->getStatusCode(), 200);
+            $this->assertEquals($session->getPage()->find('css', '.main h2')->getText(), $post->getTitle());
+            $this->assertTrue($session->getPage()->has('css', '#disqus_thread'));
+        }
     }
-    
+
     private function getPosts()
     {
         return $this->getDoctrine()->getRepository('rskaoz4Bundle:Post')->findActive();
-    }    
+    }
 }
